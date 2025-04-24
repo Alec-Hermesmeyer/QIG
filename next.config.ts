b0d7dd03439417f57189b2b87171ed6219c217cf
Alec-Hermesmeyer <ahrmsmyr@gmail.com>
2025-04-23 22:31:26 -0500

    commit

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    // Ignore TypeScript errors during build
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
